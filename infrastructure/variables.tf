8faa7edf5da8bc56d4aeebbbfebb831854af6df3
RutikGawali1 <rutikgawali1@gmail.com>
2025-08-28 19:30:46 +0530

    update

---
+++ infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+    description = "Name prefix for all resources"
+    type        = string
+    default     = "free-tier-image-pipline"
+}
+
+variable "owner_tag"{
+    description = "Owner tag for resources"
+    type        = string
+    default     = "rutik" 
+}
